032e066c109c72d304701d1ceaedd40a04e081b8
lastlink <theefunk@gmail.com>
2018-02-28 17:23:37 -0500

    update config

--- appfinish/App.js
+++ appfinish/App.js
@@ -1,7 +1,76 @@
 import React from "react";
 import Setup from "./src/boot/setup";
+const firebase = require("firebase");
+// Initialize Firebase
+
+import firebaseConfig from "./src/config";
+
+firebase.initializeApp(firebaseConfig);
+// Create a reference with .ref() instead of new Firebase(url)
+const rootRef = firebase.database().ref();
+const itemsRef = rootRef.child('items');
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      })
+    };
+    this.itemsRef = this.getRef().child('items');
+  }
+
+  getRef() {
+    return firebaseApp.database().ref();
+  }
+
+  listenForItems(itemsRef) {
+    itemsRef.on('value', (snap) => {
+
+      // get children as an array
+      var items = [];
+      snap.forEach((child) => {
+        items.push({
+          title: child.val().title,
+          _key: child.key
+        });
+      });
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+
+    });
+  }
+
+  componentDidMount() {
+    this.listenForItems(this.itemsRef);
+  }
+  listenForItems(itemsRef) {
+    itemsRef.on('value', (snap) => {
+
+      // get children as an array
+      var items = [];
+      snap.forEach((child) => {
+        items.push({
+          title: child.val().title,
+          _key: child.key
+        });
+      });
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+
+    });
+  }
+
+  componentDidMount() {
+    this.listenForItems(this.itemsRef);
+  }
+
+
   render() {
     return <Setup />;
   }
--- appfinish/src/config.template.js
+++ appfinish/src/config.template.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+export const firebaseConfig = {
     apiKey: "AIzaSyAmG9vvm5PUzVYxV_QR0KFOGyZ9sE8kKBo",
     authDomain: "fir-test-c3482.firebaseapp.com",
     databaseURL: "https://fir-test-c3482.firebaseio.com",
