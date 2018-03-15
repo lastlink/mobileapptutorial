ae15c7c614aff20d38e235fbec6fa8983d286656
lastlink <theefunk@gmail.com>
2018-03-14 20:53:55 -0400

    update progress

---
+++ app_progress_1/src/config.js
@@ -0,0 +1,18 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCVdrX4O-8WrNFBqAE0N7kA0tiKju9zTxU",
+//     authDomain: "mobileapp-7557e.firebaseapp.com",
+//     databaseURL: "https://mobileapp-7557e.firebaseio.com",
+//     projectId: "mobileapp-7557e",
+//     storageBucket: "mobileapp-7557e.appspot.com",
+//     messagingSenderId: "704008135574"
+//   };
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCVdrX4O-8WrNFBqAE0N7kA0tiKju9zTxU",
+  authDomain: "mobileapp-7557e.firebaseapp.com",
+  databaseURL: "https://mobileapp-7557e.firebaseio.com",
+  projectId: "mobileapp-7557e",
+  storageBucket: "mobileapp-7557e.appspot.com",
+  messagingSenderId: "704008135574"
+};
