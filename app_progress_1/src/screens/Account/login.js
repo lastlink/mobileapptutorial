e6c604bd9550c4ce6349fdbda26fe9be1d401d26
lastlink <theefunk@gmail.com>
2018-03-17 01:12:43 -0400

    working response

--- app_progress_1/src/screens/Account/login.js
+++ app_progress_1/src/screens/Account/login.js
@@ -13,11 +13,12 @@ import {
   Right,
   Icon,
   Form,
-  Text
+  Text,
+  Spinner
 } from "native-base";
 import styles from "./styles";
 
-import { login, resetPassword} from "../../helpers/auth";
+import { login, resetPassword } from "../../helpers/auth";
 
 function setErrorMsg(error) {
   return {
@@ -26,6 +27,43 @@ function setErrorMsg(error) {
 }
 
 class Login extends Component {
+
+  state = { loginMessage: "test" }
+
+  handleSubmit = (e) => {
+    console.log("logging in");
+    console.log(JSON.stringify(this.state))
+
+
+    if (this.state.email && this.state.pw) {
+      this.setState({ loading: true });
+      this.setState({ loginMessage: this.state.email });
+
+      login(this.state.email, this.state.pw)
+        .then((result) => {
+          this.setState({loading:false});
+          console.log("logging in")
+          console.log(result)
+
+        })
+        .catch((error) => {
+          this.setState({loading:false});
+          console.log(JSON.stringify(error))
+          this.setState(setErrorMsg('Invalid username/password.'))
+        })
+    }
+  }
+
+  resetPassword = () => {
+    resetPassword(this.email.value)
+      .then(() => this.setState(setErrorMsg(`Password reset email sent to ${this.email.value}.`)))
+      .catch((error) => this.setState(setErrorMsg(`Email address not found.`)))
+  }
+
+  handleFormFocus(event, reactNode) {
+    this.refs.scroll.scrollToFocusedInput(event, reactNode)
+  }
+
   render() {
     return (
       <Container style={styles.container}>
@@ -42,18 +80,26 @@ class Login extends Component {
         </Header>
 
         <Content>
+          <Item inlineLabel>
+            <Label>Msg:</Label>
+            <Label>{this.state.loginMessage}</Label>
+            {
+              this.state.loading &&
+              <Spinner size="large" color="#0000ff" />
+            }
+          </Item>
           <Form>
             <Item inlineLabel>
               <Label>Username</Label>
-              <Input />
+              <Input onChangeText={(text) => this.setState({ email: text })} ref={(email) => this.email = email} />
             </Item>
             <Item inlineLabel last>
               <Label>Password</Label>
-              <Input secureTextEntry />
+              <Input secureTextEntry onChangeText={(text) => this.setState({ pw: text })} ref={(pw) => this.pw = pw} />
             </Item>
           </Form>
 
-          <Button block style={{ margin: 15, marginTop: 50 }}>
+          <Button onPress={this.handleSubmit} block style={{ margin: 15, marginTop: 50 }}>
             <Text>Sign In</Text>
           </Button>
         </Content>
